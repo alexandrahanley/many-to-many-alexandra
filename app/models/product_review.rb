7179a196406992ec046a9774204c4365a07bb16a
Alexandra Hanley <alexandrahanley@gmail.com>
2015-02-25 13:54:35 -0700

    add all model relations

--- app/models/product_review.rb
+++ app/models/product_review.rb
@@ -1,4 +1,4 @@
 class ProductReview < ActiveRecord::Base
-
-
+belongs_to :product
+belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+has_many :product_reviews
+has_many :products, though: :product_reviews
+
+has_many :company_reviews
+has_many :companies, through: :company_reviews
 
 end
